cf4af8b71a25881d5b659834bda2f8ba065de50d
Destiny <dshillis1@gmail.com>
2021-08-22 12:57:40 -0600

    Clean up and mixed media page

--- js/mixedMedia.js
+++ js/mixedMedia.js
@@ -21,7 +21,7 @@ const mixedMedia = [
         medium: 'graphite on paper',
         year: '1992',
         other: '',
-        slug: './assets/img/illustration/1990s/'
+        slug: './assets/img/mixed-media/1990s/'
     },
     {
         id: '4',
@@ -29,7 +29,7 @@ const mixedMedia = [
         medium: 'pastel and color pencil on paper',
         year: '1992',
         other: '',
-        slug: './assets/img/illustration/1990s/'
+        slug: './assets/img/mixed-media/1990s/'
     },
     {
         id: '5',
@@ -93,6 +93,93 @@ const mixedMedia = [
         medium: 'Pastel and Color Pencil on Paper',
         year: '2014',
         other: '',
-        slug: './assets/img/illustration/2010s/'
+        slug: './assets/img/mixed-media/2010s/'
     }
 ]
+
+let sectionTitle = `
+<div class="d-flex justify-content-between pb-3">
+  <h3 class="section-heading text-uppercase">Mixed Media</h3>
+</div>
+`
+
+$('#sections').append(sectionTitle)
+
+if (mixedMedia.length) {
+    mixedMedia.forEach(item => {
+      const section = `
+        <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="portfolio-item">
+                <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal${item.id}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <div class="portfolio-caption-heading text-center p-3">
+                                <h4>${item.title}</h4>
+                                <button type="button" class="btn btn-outline-light mt-4">
+                                    <i class="fas fa-plus"></i> More info
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src=${item.slug}${item.id}.jpg alt="..."/>
+                </a>
+            </div>
+        </div>
+      `
+
+      const modal = `
+        <div
+          class="portfolio-modal modal fade"
+          id="portfolioModal${item.id}"
+          tabindex="-1"
+          role="dialog"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content p-3">
+              <div class="close-modal" data-bs-dismiss="modal">
+                <img src="assets/close-icon.svg" alt="Close modal" />
+              </div>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-lg-10">
+                    <div class="modal-body">
+                      <!-- Project details-->
+                      <h2 class="text-uppercase">${item.title}</h2>
+                      <div class="text-muted">
+                        ${item.medium &&
+                          `<div>
+                            ${item.medium}
+                          </div>`
+                        }
+                        ${item.year &&
+                          `<div>
+                            ${item.year}
+                          </div>`
+                        }
+                        ${item.other &&
+                          `
+                            <div class="mb-2">
+                              ${item.other}
+                            </div>
+                          `
+                        }
+                      </div>
+
+                      <img
+                        class="img-fluid d-block mx-auto"
+                        src="${item.slug}${item.id}.jpg"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      `
+
+      $('#sections').append(section)
+      $('#modals').append(modal)
+    })
+  }
--- js/painting.js
+++ js/painting.js
@@ -1272,3 +1272,90 @@ const paintings = [
         slug: './assets/img/painting/2010s/'
     },
 ]
+
+let sectionTitle = `
+    <div class="d-flex justify-content-between pb-3">
+    <h3 class="section-heading text-uppercase">Paintings</h3>
+    </div>
+`
+
+$('#sections').append(sectionTitle)
+
+if (paintings.length) {    
+    paintings.forEach(item => {
+      const section = `
+        <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="portfolio-item">
+                <a class="portfolio-link" data-bs-toggle="modal" href="#portfolioModal${item.id}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <div class="portfolio-caption-heading text-center p-3">
+                                <h4>${item.title}</h4>
+                                <button type="button" class="btn btn-outline-light mt-4">
+                                    <i class="fas fa-plus"></i> More info
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src=${item.slug}${item.id}.jpg alt="..."/>
+                </a>
+            </div>
+        </div>
+      `
+
+      const modal = `
+        <div
+          class="portfolio-modal modal fade"
+          id="portfolioModal${item.id}"
+          tabindex="-1"
+          role="dialog"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content p-3">
+              <div class="close-modal" data-bs-dismiss="modal">
+                <img src="assets/close-icon.svg" alt="Close modal" />
+              </div>
+              <div class="container">
+                <div class="row justify-content-center">
+                  <div class="col-lg-10">
+                    <div class="modal-body">
+                      <!-- Project details-->
+                      <h2 class="text-uppercase">${item.title}</h2>
+                      <div class="text-muted">
+                        ${item.medium &&
+                          `<div>
+                            ${item.medium}
+                          </div>`
+                        }
+                        ${item.year &&
+                          `<div>
+                            ${item.year}
+                          </div>`
+                        }
+                        ${item.other &&
+                          `
+                            <div class="mb-2">
+                              ${item.other}
+                            </div>
+                          `
+                        }
+                      </div>
+
+                      <img
+                        class="img-fluid d-block mx-auto"
+                        src="${item.slug}${item.id}.jpg"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      `
+
+      $('#sections').append(section)
+      $('#modals').append(modal)
+    })
+  }
